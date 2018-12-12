9d3b218d0daceff5cb5e06958859f0adc50998e8
Brian D. Boyd <bud42@users.noreply.github.com>
2018-12-12 09:56:20 -0500

    Name edited aseg as aseg.manedit.mgz
    Have to name the aseg as aseg.manedit.mgz or else it is ignored

--- spider.py
+++ spider.py
@@ -49,7 +49,7 @@ fi
 
 if [[ "${aseg}" && "${aseg}" != "None" ]]; then
    echo "Copying aseg"
-   cp "${aseg}" ${temp_dir}/Subjects/${assessor_label}/mri/aseg.presurf.mgz
+   cp "${aseg}" ${temp_dir}/Subjects/${assessor_label}/mri/aseg.manedit.mgz
 fi
 
 if [[ "${control}" && "${control}" != "None" ]]; then
